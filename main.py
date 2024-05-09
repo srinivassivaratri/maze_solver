9dee0a78d9fbc7f14efb3d6e8eb5fb22f78d57e2
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-05-10 01:10:52 +0530

    Refactor Maze class to break entrance and exit walls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graphics import Window
 from maze import Maze
+import sys
 
 
 def main():
@@ -10,9 +11,19 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    print("maze created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("Maze cannot be solved")
+    else:
+        print("Maze solved")
+    
+    # maze.solve(0, 0, num_cols -1, num_rows - 1)
 
     win.wait_for_close()
 
